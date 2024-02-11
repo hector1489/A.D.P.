c47ce3dd186c81d09e2b6bf8dd990f84ef6f6079
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-11 10:10:19 -0300

    [fix] url

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,8 +4,15 @@ const Nav = () => {
 
 
   return (
-    <div className="navbar bg-base-300">
-      <button className="btn btn-ghost text-xl">AmigoDelAlcalde</button>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Link</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
